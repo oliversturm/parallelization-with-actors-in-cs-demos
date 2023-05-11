8c057f88dc97be87a76f6aff5b7a7e3b7728e059
Oli Sturm <oliver+commitsigning@oliversturm.com>
2023-05-11 15:45:29 +0100

    fix: non-null value

--- Remoting/HelloClient/Program.cs
+++ Remoting/HelloClient/Program.cs
@@ -19,12 +19,15 @@ akka {
 }
 ");
 
+Console.WriteLine("Press RETURN to start the client");
+Console.ReadLine();
+
 using (var system = ActorSystem.Create("client", config)) {
   var server = system.ActorSelection("akka.tcp://HelloServer@127.0.0.1:8081/user/HelloActor");
 
   while (true) {
     Console.WriteLine("Please enter your name: ");
     var name = Console.ReadLine();
-    server.Tell(new Hello(name));
+    server.Tell(new Hello(name!));
   }
 }
